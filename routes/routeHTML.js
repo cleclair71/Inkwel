5e9b9709ccf2766f649b9bfe9b14393c97534ba6
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-08 17:14:15 -0500

    html route

---
+++ routes/routeHTML.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const router = require('express').Router();
+
+// HTML GET Requests
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// If no matching route is found default to home
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
