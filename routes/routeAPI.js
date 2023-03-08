021d4b743753bb76bb0fd57fb1386ca5f58288ad
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-08 17:10:31 -0500

    api routes

--- routes/routeAPI.js
+++ routes/routeAPI.js
@@ -1,11 +1,28 @@
-const fs = require('fs');
-
-// routes
+const express = require('express');
+const router = express.Router();
+const database = require('../db/database.js');
 
 // API get request
-
+router.get('/notes', (req, res) => {
+    database
+        .getNotes()
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
+});
 // API post request
-
-
+router.post('/notes', (req, res) => {
+    database
+        .addNotes(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
 // Delete the clicked note
+router.delete('/notes/:id', (req, res) => {
+    database
+        .removeNotes(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
 
