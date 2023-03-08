a142c42d7b2f24dc2d5a26765c265d9b114c04b8
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-08 16:57:42 -0500

    uuid, file structure

---
+++ routes/routeAPI.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// routes
+
+// API get request
+
+// API post request
+
+
+// Delete the clicked note
+
---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require('express');
+
+// Create Server
+const app = express();
+
+
+// server to route
+const routeAPI = require('./routes/routeAPI.js');
+const routeHTML = require('./routes/routeHTML.js');
+
+// Set Port
+const PORT = process.env.PORT || 3001;
+// Incoming array/string
+app.use(express.urlencoded({ extended: true }));
+// incoming json
+app.use(express.json());
+// Static files
+app.use(express.static('public'));
+
+// API Routes
+
+// listen
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
